49bead68d38e38186227741798ae1017324b0653
Oli Dagenais <olivida@microsoft.com>
2015-08-31 14:13:35 -0400

    Create project using maven-archetype-quickstart
    Command line:
    mvn archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes
    -DarchetypeArtifactId=maven-archetype-quickstart
    
    ...values used:
    groupId: com.microsoft.alm.java
    artifactId: git-credential-helper
    version: 1.0.0-SNAPSHOT
    package: com.microsoft.alm.java

---
+++ src/main/java/com/microsoft/alm/java/App.java
@@ -0,0 +1,13 @@
+package com.microsoft.alm.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
