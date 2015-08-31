27684bbc8c0e828f8ac28ed7b916b68d41353744
Oli Dagenais <olivida@microsoft.com>
2015-08-31 14:36:13 -0400

    Upgrade to JUnit 4.11.

--- src/test/java/com/microsoft/alm/java/AppTest.java
+++ src/test/java/com/microsoft/alm/java/AppTest.java
@@ -1,38 +1,19 @@
 package com.microsoft.alm.java;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
+    @Test public void testApp()
     {
-        assertTrue( true );
+        Assert.assertTrue( true );
     }
 }
