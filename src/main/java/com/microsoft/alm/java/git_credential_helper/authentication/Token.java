84b776d74d0c421836a9025f32ac8c7b7157a75b
Oli Dagenais <olivida@microsoft.com>
2015-08-31 23:59:18 -0400

    Add ITokenStore with minimal Token & Secret.

---
+++ src/main/java/com/microsoft/alm/java/git_credential_helper/authentication/Token.java
@@ -0,0 +1,8 @@
+package com.microsoft.alm.java.git_credential_helper.authentication;
+
+/**
+ * A security token, usually acquired by some authentication and identity services.
+ */
+public class Token extends Secret // TODO: implements IEquatable<Token>
+{
+}
