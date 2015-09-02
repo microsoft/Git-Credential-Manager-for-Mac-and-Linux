f81702821d9032162b70e41008ded513e4b4e7ee
Oli Dagenais <olivida@microsoft.com>
2015-09-02 15:55:40 -0400

    Ported OperationArguments + test.

--- src/main/java/com/microsoft/alm/java/git_credential_helper/cli/OperationArguments.java
+++ src/main/java/com/microsoft/alm/java/git_credential_helper/cli/OperationArguments.java
@@ -1,16 +1,71 @@
 package com.microsoft.alm.java.git_credential_helper.cli;
 
 import com.microsoft.alm.java.git_credential_helper.authentication.Credential;
-import com.microsoft.alm.java.git_credential_helper.helpers.NotImplementedException;
+import com.microsoft.alm.java.git_credential_helper.helpers.Debug;
+import com.microsoft.alm.java.git_credential_helper.helpers.ObjectExtensions;
+import com.microsoft.alm.java.git_credential_helper.helpers.StringHelper;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 
 final class OperationArguments
 {
-    OperationArguments(final BufferedReader stdin)
+    OperationArguments(final BufferedReader stdin) throws IOException, URISyntaxException
     {
-        throw new NotImplementedException();
+        Debug.Assert(stdin != null, "The stdin parameter is null");
+
+        this.Authority = AuthorityType.Auto;
+        this.Interactivity = com.microsoft.alm.java.git_credential_helper.cli.Interactivity.Auto;
+        this.ValidateCredentials = true;
+        this.WriteLog = false;
+
+        String protocol = null;
+        String host = null;
+        String path = null;
+        String line;
+        while (!StringHelper.isNullOrWhiteSpace((line = stdin.readLine())))
+        {
+            String[] pair = line.split("=", 2);
+
+            if (pair.length == 2)
+            {
+                // Java doesn't support a switch on a String!
+                if ("protocol".equals(pair[0]))
+                {
+                    protocol = pair[1];
+                }
+                else if ("host".equals(pair[0]))
+                {
+                    host = pair[1];
+                }
+                else if ("path".equals(pair[0]))
+                {
+                    path = pair[1];
+                }
+                else if ("username".equals(pair[0]))
+                {
+                    userName = pair[1];
+                }
+                else if ("password".equals(pair[0]))
+                {
+                    password = pair[1];
+                }
+            }
+        }
+        Protocol = protocol;
+        Host = host;
+        Path = path;
+
+        if (this.Protocol != null && this.Host != null)
+        {
+            this.TargetUri = new URI(String.format("%1$s://%2$s/", this.Protocol, this.Host));
+        }
+        else
+        {
+            this.TargetUri = null;
+        }
     }
 
     public final String Protocol;
@@ -18,64 +73,53 @@ final class OperationArguments
     public final String Path;
     public final URI TargetUri;
 
+    private String userName;
     public String getUserName()
     {
-        throw new NotImplementedException();
+        return userName;
     }
 
+    private String password;
     public String getPassword()
     {
-        throw new NotImplementedException();
+        return password;
     }
 
-    public AuthorityType getAuthority()
-    {
-        throw new NotImplementedException();
-    }
+    public AuthorityType Authority;
 
-    public void setAuthority(final AuthorityType value)
-    {
-        throw new NotImplementedException();
-    }
+    public Interactivity Interactivity;
 
-    public Interactivity getInteractivity()
-    {
-        throw new NotImplementedException();
-    }
+    public boolean ValidateCredentials;
 
-    public void setInteractivity(final Interactivity value)
-    {
-        throw new NotImplementedException();
-    }
-
-    public boolean getValidateCredentials()
-    {
-        throw new NotImplementedException();
-    }
-
-    public void setValidateCredentials(final boolean value)
-    {
-        throw new NotImplementedException();
-    }
-
-    public boolean getWriteLog()
-    {
-        throw new NotImplementedException();
-    }
-
-    public void setWriteLog(final boolean value)
-    {
-        throw new NotImplementedException();
-    }
+    public boolean WriteLog;
 
     public void setCredentials(final Credential credentials)
     {
-        throw new NotImplementedException();
+        this.userName = credentials.Username;
+        this.password = credentials.Password;
     }
 
     @Override
     public String toString()
     {
-        throw new NotImplementedException();
+        final StringBuilder builder = new StringBuilder();
+
+        builder.append("protocol=")
+                .append(ObjectExtensions.coalesce(this.Protocol, StringHelper.Empty))
+                .append("\n");
+        builder.append("host=")
+                .append(ObjectExtensions.coalesce(this.Host, StringHelper.Empty))
+                .append("\n");
+        builder.append("path=")
+                .append(ObjectExtensions.coalesce(this.Path, StringHelper.Empty))
+                .append("\n");
+        builder.append("username=")
+                .append(ObjectExtensions.coalesce(this.userName, StringHelper.Empty))
+                .append("\n");
+        builder.append("password=")
+                .append(ObjectExtensions.coalesce(this.password, StringHelper.Empty))
+                .append("\n");
+
+        return builder.toString();
     }
 }
