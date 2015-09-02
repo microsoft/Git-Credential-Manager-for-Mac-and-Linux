464c32746f699cdd31fcc8413652b2a73d62d780
Oli Dagenais <olivida@microsoft.com>
2015-09-01 20:57:13 -0400

    Add ICredentialStore.

---
+++ src/main/java/com/microsoft/alm/java/git_credential_helper/authentication/ICredentialStore.java
@@ -0,0 +1,11 @@
+package com.microsoft.alm.java.git_credential_helper.authentication;
+
+import java.net.URI;
+import java.util.concurrent.atomic.AtomicReference;
+
+public interface ICredentialStore
+{
+    void deleteCredentials(final URI targetUri);
+    void readCredentials(final URI targetUri, final AtomicReference<Credential> credentials);
+    void writeCredentials(final URI targetUri, final Credential credentials);
+}
