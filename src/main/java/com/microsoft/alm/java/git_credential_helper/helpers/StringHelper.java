6deb6a5b1a4939e5e0752d59fb37a601fe0a2cbe
Oli Dagenais <olivida@microsoft.com>
2015-09-03 15:38:21 -0400

    Add StringHelper.isNullOrEmpty().

--- src/main/java/com/microsoft/alm/java/git_credential_helper/helpers/StringHelper.java
+++ src/main/java/com/microsoft/alm/java/git_credential_helper/helpers/StringHelper.java
@@ -4,6 +4,11 @@ public class StringHelper
 {
     public static final String Empty = "";
 
+    public static boolean isNullOrEmpty(final String s)
+    {
+        return null == s || (s.length() == 0);
+    }
+
     public static boolean isNullOrWhiteSpace(final String s)
     {
         return null == s || (s.trim().length() == 0);
