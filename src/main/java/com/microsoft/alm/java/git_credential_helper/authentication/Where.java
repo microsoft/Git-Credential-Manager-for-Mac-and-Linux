fc63785b3015f31541b6327e36cf99ded34468b8
Oli Dagenais <olivida@microsoft.com>
2015-09-03 12:33:58 -0400

    Preliminary port of Where.git*Config() methods.

--- src/main/java/com/microsoft/alm/java/git_credential_helper/authentication/Where.java
+++ src/main/java/com/microsoft/alm/java/git_credential_helper/authentication/Where.java
@@ -1,15 +1,25 @@
 package com.microsoft.alm.java.git_credential_helper.authentication;
 
+import com.microsoft.alm.java.git_credential_helper.helpers.Environment;
 import com.microsoft.alm.java.git_credential_helper.helpers.Func;
-import com.microsoft.alm.java.git_credential_helper.helpers.NotImplementedException;
+import com.microsoft.alm.java.git_credential_helper.helpers.IteratorExtensions;
 import com.microsoft.alm.java.git_credential_helper.helpers.ObjectExtensions;
+import com.microsoft.alm.java.git_credential_helper.helpers.Path;
 import com.microsoft.alm.java.git_credential_helper.helpers.StringHelper;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.NameFileFilter;
+import org.apache.commons.io.filefilter.TrueFileFilter;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Where
 {
+    private static final Pattern gitdirPattern = Pattern.compile("gitdir\\s*:\\s([^\\r\\n]+)", Pattern.CASE_INSENSITIVE);
+
     /**
      * Finds the "best" path to an app of a given name.
      *
@@ -83,7 +93,18 @@ public class Where
      */
     public static boolean gitGlobalConfig(final AtomicReference<String> path)
     {
-        throw new NotImplementedException();
+        final String GlobalConfigFileName = ".gitconfig";
+
+        path.set(null);
+
+        String globalPath = Path.combine(Environment.getFolderPath(Environment.SpecialFolder.UserProfile), GlobalConfigFileName);
+
+        if (Path.fileExists(globalPath))
+        {
+            path.set(globalPath);
+        }
+
+        return path.get() != null;
     }
 
     /**
@@ -93,19 +114,93 @@ public class Where
      * @param path Path to the Git local configuration.
      * @return True if succeeds; false otherwise.
      */
-    public static boolean gitLocalConfig(final String startingDirectory, final AtomicReference<String> path)
+    public static boolean gitLocalConfig(final String startingDirectory, final AtomicReference<String> path) throws IOException
     {
-        throw new NotImplementedException();
+        final String GitOdbFolderName = ".git";
+        final String LocalConfigFileName = "config";
+
+        path.set(null);
+
+        if (!StringHelper.isNullOrWhiteSpace(startingDirectory))
+        {
+            File dir = new File(startingDirectory);
+            if (dir.exists())
+
+            {
+                // the hasOdb Func was simplified to an exists() check
+                File result = null;
+                while (dir != null && dir.exists() && dir.getParentFile() != null && dir.getParentFile().exists())
+                {
+                    result = new File(dir, GitOdbFolderName);
+                    if (result.exists())
+                        break;
+
+                    dir = dir.getParentFile();
+                }
+
+                if (result != null && result.exists())
+                {
+                    if (result.isDirectory())
+                    {
+                        final String localPath = Path.combine(result.getAbsolutePath(), LocalConfigFileName);
+                        if (Path.fileExists(localPath))
+                        {
+                            path.set(localPath);
+                        }
+                    }
+                    else
+                    {
+                        // parse the file like gitdir: ../.git/modules/libgit2sharp
+                        String content = null;
+
+                        // shortcut the opening streams & readers just to read the whole file as a string
+                        content = FileUtils.readFileToString(result);
+
+                        final Matcher match;
+                        if ((match = gitdirPattern.matcher(content)).matches()
+                            && match.groupCount() > 1)
+                        {
+                            content = match.group(1);
+                            // don't replace / with \\
+
+                            String localPath = null;
+
+                            if (Path.isAbsolute(content))
+                            {
+                                localPath = content;
+                            }
+                            else
+                            {
+                                localPath = Path.getDirectoryName(result.getAbsolutePath());
+                                localPath = Path.combine(localPath, content);
+                            }
+
+                            if (Path.directoryExists(localPath))
+                            {
+                                localPath = Path.combine(localPath, LocalConfigFileName);
+                                if (Path.fileExists(localPath))
+                                {
+                                    path.set(localPath);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return path.get() != null;
     }
+
     /**
      * Gets the path to the Git local configuration file based on the current working directory.
      *
      * @param path Path to the Git local configuration.
      * @return True if succeeds; false otherwise.
      */
-    public static boolean gitLocalConfig(final AtomicReference<String> path)
+    public static boolean gitLocalConfig(final AtomicReference<String> path) throws IOException
     {
-        throw new NotImplementedException();
+        return gitLocalConfig(Environment.getCurrentDirectory(), path);
     }
 
     /**
@@ -116,6 +211,27 @@ public class Where
      */
     public static boolean gitSystemConfig(final AtomicReference<String> path)
     {
-        throw new NotImplementedException();
+        final String SystemConfigFileName = "gitconfig";
+
+        // find Git on the local disk - the system config is stored relative to it
+        if (app("git", path))
+        {
+            File gitInfo = new File(path.get());
+            File dir = gitInfo.getParentFile();
+            if (dir.getParentFile() != null)
+            {
+                dir = dir.getParentFile();
+            }
+
+            File file = IteratorExtensions.firstOrDefault(FileUtils.iterateFiles(dir, new NameFileFilter(SystemConfigFileName), TrueFileFilter.INSTANCE));
+            if (file != null && file.exists())
+            {
+                path.set(file.getAbsolutePath());
+                return true;
+            }
+        }
+
+        path.set(null);
+        return false;
     }
 }
