f6b607b8e570d8f6c3917e8720259624a2af0ff7
Oli Dagenais <olivida@microsoft.com>
2015-09-09 17:29:08 -0400

    Add Guid helper class with Empty constant.

---
+++ src/main/java/com/microsoft/alm/java/git_credential_helper/helpers/Guid.java
@@ -0,0 +1,8 @@
+package com.microsoft.alm.java.git_credential_helper.helpers;
+
+import java.util.UUID;
+
+public class Guid
+{
+    public static final UUID Empty = UUID.fromString("00000000-0000-0000-0000-000000000000");
+}
