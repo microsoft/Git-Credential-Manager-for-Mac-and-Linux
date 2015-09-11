48f7e8d4d2607268daaae604cd9faa44941f14bd
Oli Dagenais <olivida@microsoft.com>
2015-09-11 16:18:28 -0400

    Add descriptions to TokenType.

--- src/main/java/com/microsoft/alm/java/git_credential_helper/authentication/TokenType.java
+++ src/main/java/com/microsoft/alm/java/git_credential_helper/authentication/TokenType.java
@@ -2,25 +2,38 @@ package com.microsoft.alm.java.git_credential_helper.authentication;
 
 public enum TokenType
 {
-    Unknown,
+    Unknown(null),
     /**
      * Azure Directory Access Token
      */
-    Access,
+    Access("Azure Directory Access Token"),
     /**
      * Azure Directory Refresh Token
      */
-    Refresh,
+    Refresh("Azure Directory Refresh Token"),
     /**
      * Personal Access Token, can be compact or not.
      */
-    Personal,
+    Personal("Personal Access Token"),
     /**
      * Federated Authentication (aka FedAuth) Token
      */
-    Federated,
+    Federated("Federated Authentication Token"),
     /**
      * Used only for testing
      */
-    Test,
+    Test("Test-only Token"),
+    ;
+
+    private final String description;
+
+    private TokenType(final String description)
+    {
+        this.description = description;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
 }
