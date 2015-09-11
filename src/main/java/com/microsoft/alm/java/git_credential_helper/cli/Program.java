70c591ac4fc8117065525c38fc12c867badfeda6
Oli Dagenais <olivida@microsoft.com>
2015-09-11 13:46:31 -0400

    Add testing seam with standard I/O fields.
    Action methods will now operate on standardIn and standardOut, making it
    easier to test without launching a sub-process and capturing its
    handles.
    
    innerMain() was also extracted to not include the catch blocks, making
    it easier to test error conditions.

--- src/main/java/com/microsoft/alm/java/git_credential_helper/cli/Program.java
+++ src/main/java/com/microsoft/alm/java/git_credential_helper/cli/Program.java
@@ -7,6 +7,8 @@ import com.microsoft.alm.java.git_credential_helper.helpers.NotImplementedExcept
 import com.microsoft.alm.java.git_credential_helper.helpers.Trace;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.concurrent.Callable;
@@ -16,35 +18,17 @@ public class Program
 {
     private static final String ConfigPrefix = "credential";
 
+    private final InputStream standardIn;
+    private final PrintStream standardOut;
+
     public static void main(final String[] args)
     {
         try
         {
             enableDebugTrace();
-            
-            if (args.length == 0 || args[0].contains("?"))
-            {
-                printHelpMessage();
-                return;
-            }
+            final Program program = new Program(System.in, System.out);
 
-            // list of arg => method associations (case-insensitive)
-            final Map<String, Callable<Void>> actions = new TreeMap<String, Callable<Void>>(String.CASE_INSENSITIVE_ORDER);
-            actions.put("approve", Store);
-            actions.put("erase", Erase);
-            actions.put("fill", Get);
-            actions.put("get", Get);
-            actions.put("reject", Erase);
-            actions.put("store", Store);
-            actions.put("version", PrintVersion);
-
-            for (final String arg : args)
-            {
-                if (actions.containsKey(arg))
-                {
-                    actions.get(arg).call();
-                }
-            }
+            program.innerMain(args);
         }
         catch (final Exception exception)
         {
@@ -56,47 +40,80 @@ public class Program
         Trace.flush();
     }
 
-    private static void printHelpMessage()
+    void innerMain(String[] args) throws Exception
+    {
+        if (args.length == 0 || args[0].contains("?"))
+        {
+            printHelpMessage();
+            return;
+        }
+
+        // list of arg => method associations (case-insensitive)
+        final Map<String, Callable<Void>> actions = new TreeMap<String, Callable<Void>>(String.CASE_INSENSITIVE_ORDER);
+        actions.put("approve", Store);
+        actions.put("erase", Erase);
+        actions.put("fill", Get);
+        actions.put("get", Get);
+        actions.put("reject", Erase);
+        actions.put("store", Store);
+        actions.put("version", PrintVersion);
+
+        for (final String arg : args)
+        {
+            if (actions.containsKey(arg))
+            {
+                actions.get(arg).call();
+            }
+        }
+    }
+
+    public Program(final InputStream standardIn, final PrintStream standardOut)
+    {
+        this.standardIn = standardIn;
+        this.standardOut = standardOut;
+    }
+
+    private void printHelpMessage()
     {
         Trace.writeLine("Program::printHelpMessage");
 
-        System.out.println("usage: git credential <command> [<args>]");
-        System.out.println();
-        System.out.println("   authority      Defines the type of authentication to be used.");
-        System.out.println("                  Supports Auto, Basic, AAD, MSA, and Integrated.");
-        System.out.println("                  Default is Auto.");
-        System.out.println();
-        System.out.println("      `git config --global credential.microsoft.visualstudio.com.authority AAD`");
-        System.out.println();
-        System.out.println("   interactive    Specifies if user can be prompted for credentials or not.");
-        System.out.println("                  Supports Auto, Always, or Never. Defaults to Auto.");
-        System.out.println("                  Only used by AAD and MSA authority.");
-        System.out.println();
-        System.out.println("      `git config --global credential.microsoft.visualstudio.com.interactive never`");
-        System.out.println();
-        System.out.println("   validate       Causes validation of credentials before supplying them");
-        System.out.println("                  to Git. Invalid credentials get a refresh attempt");
-        System.out.println("                  before failing. Incurs some minor overhead.");
-        System.out.println("                  Defaults to TRUE. Ignored by Basic authority.");
-        System.out.println();
-        System.out.println("      `git config --global credential.microsoft.visualstudio.com.validate false`");
-        System.out.println();
-        System.out.println("   writelog       Enables trace logging of all activities. Logs are written to");
-        System.out.println("                  the .git/ folder at the root of the repository.");
-        System.out.println("                  Defaults to FALSE.");
-        System.out.println();
-        System.out.println("      `git config --global credential.writelog true`");
-        System.out.println();
-        System.out.println("Sample Configuration:");
-        System.out.println("   [credential \"microsoft.visualstudio.com\"]");
-        System.out.println("       authority = AAD");
-        System.out.println("   [credential \"visualstudio.com\"]");
-        System.out.println("       authority = MSA");
-        System.out.println("   [credential]");
-        System.out.println("       helper = manager");
+        standardOut.println("usage: git credential <command> [<args>]");
+        standardOut.println();
+        standardOut.println("   authority      Defines the type of authentication to be used.");
+        standardOut.println("                  Supports Auto, Basic, AAD, MSA, and Integrated.");
+        standardOut.println("                  Default is Auto.");
+        standardOut.println();
+        standardOut.println("      `git config --global credential.microsoft.visualstudio.com.authority AAD`");
+        standardOut.println();
+        standardOut.println("   interactive    Specifies if user can be prompted for credentials or not.");
+        standardOut.println("                  Supports Auto, Always, or Never. Defaults to Auto.");
+        standardOut.println("                  Only used by AAD and MSA authority.");
+        standardOut.println();
+        standardOut.println("      `git config --global credential.microsoft.visualstudio.com.interactive never`");
+        standardOut.println();
+        standardOut.println("   validate       Causes validation of credentials before supplying them");
+        standardOut.println("                  to Git. Invalid credentials get a refresh attempt");
+        standardOut.println("                  before failing. Incurs some minor overhead.");
+        standardOut.println("                  Defaults to TRUE. Ignored by Basic authority.");
+        standardOut.println();
+        standardOut.println("      `git config --global credential.microsoft.visualstudio.com.validate false`");
+        standardOut.println();
+        standardOut.println("   writelog       Enables trace logging of all activities. Logs are written to");
+        standardOut.println("                  the .git/ folder at the root of the repository.");
+        standardOut.println("                  Defaults to FALSE.");
+        standardOut.println();
+        standardOut.println("      `git config --global credential.writelog true`");
+        standardOut.println();
+        standardOut.println("Sample Configuration:");
+        standardOut.println("   [credential \"microsoft.visualstudio.com\"]");
+        standardOut.println("       authority = AAD");
+        standardOut.println("   [credential \"visualstudio.com\"]");
+        standardOut.println("       authority = MSA");
+        standardOut.println("   [credential]");
+        standardOut.println("       helper = manager");
     }
 
-    private static final Callable<Void> Erase = new Callable<Void>()
+    private final Callable<Void> Erase = new Callable<Void>()
     {
         @Override public Void call()
         {
@@ -104,12 +121,12 @@ public class Program
             return null;
         }
     };
-    private static void erase()
+    private void erase()
     {
         throw new NotImplementedException();
     }
 
-    private static final Callable<Void> Get = new Callable<Void>()
+    private final Callable<Void> Get = new Callable<Void>()
     {
         @Override public Void call()
         {
@@ -117,12 +134,12 @@ public class Program
             return null;
         }
     };
-    private static void get()
+    private void get()
     {
         throw new NotImplementedException();
     }
 
-    private static final Callable<Void> Store = new Callable<Void>()
+    private final Callable<Void> Store = new Callable<Void>()
     {
         @Override public Void call()
         {
@@ -130,12 +147,12 @@ public class Program
             return null;
         }
     };
-    private static void store()
+    private void store()
     {
         throw new NotImplementedException();
     }
 
-    private static final Callable<Void> PrintVersion = new Callable<Void>()
+    private final Callable<Void> PrintVersion = new Callable<Void>()
     {
         @Override public Void call()
         {
@@ -143,7 +160,7 @@ public class Program
             return null;
         }
     };
-    private static void printVersion()
+    private void printVersion()
     {
         throw new NotImplementedException();
     }
