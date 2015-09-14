d2a7094678b9f65dffb2c637fddb9da4f87b6647
Oli Dagenais <olivida@microsoft.com>
2015-09-14 13:00:56 -0400

    Add Guid.toBytes() and tests

--- src/main/java/com/microsoft/alm/java/git_credential_helper/helpers/Guid.java
+++ src/main/java/com/microsoft/alm/java/git_credential_helper/helpers/Guid.java
@@ -1,8 +1,30 @@
 package com.microsoft.alm.java.git_credential_helper.helpers;
 
+import java.nio.ByteBuffer;
 import java.util.UUID;
 
 public class Guid
 {
     public static final UUID Empty = UUID.fromString("00000000-0000-0000-0000-000000000000");
+
+    // Inspired by: http://stackoverflow.com/a/1055668/
+    public static byte[] toBytes(final UUID value)
+    {
+        final ByteBuffer bytes = ByteBuffer.allocate(16);
+
+        final long mostSignificantBits = value.getMostSignificantBits();
+
+        final int upperMsb = (int) (mostSignificantBits >> 32);
+        bytes.putInt(Integer.reverseBytes(upperMsb));
+
+        final int lowerMsb = (int) (mostSignificantBits & 0xffffffff);
+        final short firstLowerMsb = (short) (lowerMsb >> 16);
+        bytes.putShort(Short.reverseBytes(firstLowerMsb));
+        final short secondLowerMsb = (short) (lowerMsb & 0xffff);
+        bytes.putShort(Short.reverseBytes(secondLowerMsb));
+
+        bytes.putLong(value.getLeastSignificantBits());
+
+        return bytes.array();
+    }
 }
---
+++ src/test/java/com/microsoft/alm/java/git_credential_helper/helpers/GuidTest.java
@@ -0,0 +1,73 @@
+package com.microsoft.alm.java.git_credential_helper.helpers;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.UUID;
+
+public class GuidTest
+{
+
+    @Test public void toBytes_allOnes() throws Exception
+    {
+        final UUID value = UUID.fromString("ffffffff-ffff-ffff-ffff-ffffffffffff");
+        final byte[] actual = Guid.toBytes(value);
+
+        byte[] expected =
+        {
+            (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff,
+            (byte) 0xff, (byte) 0xff,
+            (byte) 0xff, (byte) 0xff,
+            (byte) 0xff, (byte) 0xff,
+            (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff,
+        };
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test public void toBytes_allZeroes() throws Exception
+    {
+        final byte[] actual = Guid.toBytes(Guid.Empty);
+
+        byte[] expected =
+        {
+            0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00,
+            0x00, 0x00,
+            0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        };
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test public void toBytes_typical() throws Exception
+    {
+        final UUID value = UUID.fromString("8602283e-2ed6-4960-adaa-97be7d9913de");
+        final byte[] actual = Guid.toBytes(value);
+
+        byte[] expected =
+        {
+            (byte) 0x3e, (byte) 0x28, (byte) 0x02, (byte) 0x86,
+            (byte) 0xd6, (byte) 0x2e,
+            (byte) 0x60, (byte) 0x49,
+            (byte) 0xad, (byte) 0xaa,
+            (byte) 0x97, (byte) 0xbe, (byte) 0x7d, (byte) 0x99, (byte) 0x13, (byte) 0xde,
+        };
+        assertArrayEquals(expected, actual);
+    }
+
+    private static void assertArrayEquals(byte[] expected, byte[] actual)
+    {
+        if (!Arrays.equals(expected, actual))
+        {
+            final String template = "Arrays were different.\n" +
+                    "Expected :%1$s\n" +
+                    "Actual   :%2$s";
+            final String expectedHex = BitConverter.toString(expected);
+            final String actualHex = BitConverter.toString(actual);
+            final String message = String.format(template, expectedHex, actualHex);
+            Assert.fail(message);
+        }
+    }
+}
