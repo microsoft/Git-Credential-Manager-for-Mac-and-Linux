2bd1a0ff2222fc295c71acceac3f1fb4692f460a
Oli Dagenais <olivida@microsoft.com>
2015-09-14 15:47:40 -0400

    Add Guid.fromBytes() helper & tests.

--- src/test/java/com/microsoft/alm/java/git_credential_helper/helpers/GuidTest.java
+++ src/test/java/com/microsoft/alm/java/git_credential_helper/helpers/GuidTest.java
@@ -9,6 +9,53 @@ import java.util.UUID;
 
 public class GuidTest
 {
+    @Test public void fromBytes_allOnes() throws Exception
+    {
+        byte[] input =
+        {
+            (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff,
+            (byte) 0xff, (byte) 0xff,
+            (byte) 0xff, (byte) 0xff,
+            (byte) 0xff, (byte) 0xff,
+            (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff,
+        };
+
+        final UUID actual = Guid.fromBytes(input);
+
+        Assert.assertEquals("ffffffff-ffff-ffff-ffff-ffffffffffff", actual.toString());
+    }
+
+    @Test public void fromBytes_allZeroes() throws Exception
+    {
+        byte[] input =
+        {
+            (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+            (byte) 0x00, (byte) 0x00,
+            (byte) 0x00, (byte) 0x00,
+            (byte) 0x00, (byte) 0x00,
+            (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+        };
+
+        final UUID actual = Guid.fromBytes(input);
+
+        Assert.assertEquals("00000000-0000-0000-0000-000000000000", actual.toString());
+    }
+
+    @Test public void fromBytes_typical() throws Exception
+    {
+        byte[] input =
+        {
+            (byte) 0x3e, (byte) 0x28, (byte) 0x02, (byte) 0x86,
+            (byte) 0xd6, (byte) 0x2e,
+            (byte) 0x60, (byte) 0x49,
+            (byte) 0xad, (byte) 0xaa,
+            (byte) 0x97, (byte) 0xbe, (byte) 0x7d, (byte) 0x99, (byte) 0x13, (byte) 0xde,
+        };
+
+        final UUID actual = Guid.fromBytes(input);
+
+        Assert.assertEquals("8602283e-2ed6-4960-adaa-97be7d9913de", actual.toString());
+    }
 
     @Test public void toBytes_allOnes() throws Exception
     {
