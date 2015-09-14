36560b0ac65798e38ba77dd854717e4cadf4d794
Oli Dagenais <olivida@microsoft.com>
2015-09-14 12:59:54 -0400

    Add BitConverter helper with test

---
+++ src/test/java/com/microsoft/alm/java/git_credential_helper/helpers/BitConverterTest.java
@@ -0,0 +1,16 @@
+package com.microsoft.alm.java.git_credential_helper.helpers;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BitConverterTest
+{
+    @Test public void toString_example() throws Exception
+    {
+        final byte[] bytes = {0x7f, 0x2c, 0x4a, 0x00};
+
+        final String actual = BitConverter.toString(bytes);
+
+        Assert.assertEquals("7F-2C-4A-00", actual);
+    }
+}
