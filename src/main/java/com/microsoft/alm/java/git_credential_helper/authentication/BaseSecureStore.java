93d1cdc05b1bb2f7715da2301f2217c6e9fbd941
Oli Dagenais <olivida@microsoft.com>
2015-09-14 23:48:26 -0400

    Finish initial BaseSecureStore port

--- src/main/java/com/microsoft/alm/java/git_credential_helper/authentication/BaseSecureStore.java
+++ src/main/java/com/microsoft/alm/java/git_credential_helper/authentication/BaseSecureStore.java
@@ -1,7 +1,9 @@
 package com.microsoft.alm.java.git_credential_helper.authentication;
 
+import com.microsoft.alm.java.git_credential_helper.helpers.Debug;
 import com.microsoft.alm.java.git_credential_helper.helpers.ISecureStore;
 import com.microsoft.alm.java.git_credential_helper.helpers.NotImplementedException;
+import com.microsoft.alm.java.git_credential_helper.helpers.Trace;
 
 import java.net.URI;
 
@@ -18,33 +20,53 @@ public abstract class BaseSecureStore
 
     protected void delete(final String targetName)
     {
-        delegate.delete(targetName);
+        Trace.writeLine("BaseSecureStore::delete");
+
+        try
+        {
+            delegate.delete(targetName);
+        }
+        catch (final Throwable throwable)
+        {
+            Trace.writeLine(throwable.toString());
+        }
     }
 
     protected abstract String getTargetName(final URI targetUri);
 
     protected Credential readCredentials(final String targetName)
     {
+        Trace.writeLine("BaseSecureStore::readCredentials");
+
         return delegate.readCredentials(targetName);
     }
 
     protected Token readToken(final String targetName)
     {
+        Trace.writeLine("BaseSecureStore::readToken");
+
         return delegate.readToken(targetName);
     }
 
     protected void writeCredential(final String targetName, final Credential credentials)
     {
+        Trace.writeLine("BaseSecureStore::writeCredential");
+
         delegate.writeCredential(targetName,  credentials);
     }
 
     protected void writeToken(final String targetName, final Token token)
     {
+        Trace.writeLine("BaseSecureStore::writeToken");
+
         delegate.writeToken(targetName, token);
     }
 
     static void validateTargetUri(final URI targetUri)
     {
-        throw new NotImplementedException();
+        if (targetUri == null)
+            throw new IllegalArgumentException("targetUri");
+        if (!targetUri.isAbsolute())
+            throw new IllegalArgumentException("The target URI must be an absolute URI");
     }
 }
