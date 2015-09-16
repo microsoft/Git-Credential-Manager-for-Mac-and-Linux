38bc68ca8d771b6fe99c545c14c484f9da55f388
Oli Dagenais <olivida@microsoft.com>
2015-09-16 13:39:25 -0400

    Add Path.changeExtension() helper method and tests

--- src/main/java/com/microsoft/alm/java/git_credential_helper/helpers/Path.java
+++ src/main/java/com/microsoft/alm/java/git_credential_helper/helpers/Path.java
@@ -7,6 +7,27 @@ import java.io.File;
  */
 public class Path
 {
+    public static String changeExtension(final String path, final String extension)
+    {
+        if (StringHelper.isNullOrEmpty(path))
+        {
+            return path;
+        }
+        final StringBuilder sb = new StringBuilder(path.length());
+        final String minusLastExtension = getFileNameWithoutExtension(path);
+        sb.append(minusLastExtension);
+
+        if (extension != null)
+        {
+            if (!extension.startsWith(".") && !path.endsWith("."))
+            {
+                sb.append('.');
+            }
+            sb.append(extension);
+        }
+        return sb.toString();
+    }
+
     public static String combine(final String path1, final String path2)
     {
         final File file = new File(path1, path2);
@@ -29,6 +50,13 @@ public class Path
     {
         final File file = new File(path);
         return file.getParent();
+    }
+
+    // Inspired by http://stackoverflow.com/a/4094034
+    public static String getFileNameWithoutExtension(final String path)
+    {
+        final String result = path.replaceAll("^(.*[/\\\\]?[^/\\\\]*)(\\.[^./\\\\]+)$", "$1");
+        return result;
     }
 
     public static boolean isAbsolute(final String path)
