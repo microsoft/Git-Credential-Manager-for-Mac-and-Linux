cc11683236f61eaeadccf735bc81d933b080e478
Oli Dagenais <olivida@microsoft.com>
2015-09-17 11:26:10 -0400

    Expand IVso[Aad/Msa]Authentication interfaces
    This will make it easier to generalize & mock out implementations.

--- src/main/java/com/microsoft/alm/java/git_credential_helper/authentication/IVsoMsaAuthentication.java
+++ src/main/java/com/microsoft/alm/java/git_credential_helper/authentication/IVsoMsaAuthentication.java
@@ -1,8 +1,11 @@
 package com.microsoft.alm.java.git_credential_helper.authentication;
 
 import java.net.URI;
+import java.util.concurrent.Future;
 
-public interface IVsoMsaAuthentication
+public interface IVsoMsaAuthentication extends IAuthentication
 {
     boolean interactiveLogon(final URI targetUri, boolean requestCompactToken);
+    Future<Boolean> refreshCredentials(final URI targetUri, final boolean requireCompactToken);
+    Future<Boolean> validateCredentials(final URI targetUri, final Credential credentials);
 }
