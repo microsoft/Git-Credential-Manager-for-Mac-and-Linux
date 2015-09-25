b4c74389187ba139d09399206228ed8564699e94
Oli Dagenais <olivida@microsoft.com>
2015-09-25 15:16:34 -0400

    Add License file, enforce license header

--- src/main/java/com/microsoft/alm/authentication/Global.java
+++ src/main/java/com/microsoft/alm/authentication/Global.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root.
+
 package com.microsoft.alm.authentication;
 
 import org.apache.commons.logging.impl.Jdk13LumberjackLogger;
--- src/test/java/com/microsoft/alm/gitcredentialhelper/OperationArgumentsTest.java
+++ src/test/java/com/microsoft/alm/gitcredentialhelper/OperationArgumentsTest.java
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root.
+
 package com.microsoft.alm.gitcredentialhelper;
 
 import org.junit.Assert;
