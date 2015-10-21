3e8b0c7e8dc99b8d8053103c692f3091eb3e2183
Oli Dagenais <olivida@microsoft.com>
2015-10-21 12:21:34 -0400

    Add failing manual end-to-end test of "get"
    This simulates Git sending to the process' stdin and asserts the
    process' stdout.  Much easier to set breakpoints this way.

--- src/test/java/com/microsoft/alm/gitcredentialhelper/ProgramTest.java
+++ src/test/java/com/microsoft/alm/gitcredentialhelper/ProgramTest.java
@@ -3,17 +3,68 @@
 
 package com.microsoft.alm.gitcredentialhelper;
 
+import com.microsoft.alm.authentication.Configuration;
+import com.microsoft.alm.authentication.IAuthentication;
+import com.microsoft.alm.authentication.ISecureStore;
+import com.microsoft.alm.helpers.Environment;
+import com.microsoft.alm.helpers.Trace;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
 
 public class ProgramTest
 {
 
-    /**
-     * Rigourous Test :-)
-     */
-    @Test public void testApp()
+    @Ignore("This test requires user intervention and must be run manually.")
+    @Test public void get() throws Exception
     {
-        Assert.assertTrue( true );
+        Trace.getListeners().add(System.err);
+        final String hostAccount = System.getProperty("hostAccount");
+        final String input = "protocol=https\n" +
+            "host=" + hostAccount + ".visualstudio.com\n" +
+            "path=\n" +
+            "";
+        final InputStream inputStream = new ByteArrayInputStream(input.getBytes("UTF-8"));
+        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        final Program program = new Program(inputStream, new PrintStream(outputStream), new IComponentFactory()
+        {
+            @Override
+            public IAuthentication createAuthentication(final OperationArguments operationArguments, final ISecureStore secureStore)
+            {
+                return Program.createAuthentication(operationArguments, secureStore);
+            }
+
+            @Override
+            public Configuration createConfiguration() throws IOException
+            {
+                return new Configuration();
+            }
+
+            @Override
+            public ISecureStore createSecureStore()
+            {
+                final String home = Environment.getFolderPath(Environment.SpecialFolder.UserProfile);
+                final File insecureFile = new File(home, "insecureStore.xml");
+                return new InsecureStore(insecureFile);
+            }
+        });
+
+        program.innerMain(new String[]{"get"});
+
+        final String output = outputStream.toString("UTF-8");
+        final String expected = "protocol=https\n" +
+            "host=" + hostAccount + ".visualstudio.com\n" +
+            "path=\n" +
+            "username=Personal Access Token\n";
+        Assert.assertTrue(output.startsWith(expected));
+        Assert.assertTrue(output.contains("password="));
     }
+
 }
