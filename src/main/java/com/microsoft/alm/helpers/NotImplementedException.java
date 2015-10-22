092f5e476a7e88f144c365942085b2b8fcbad195
Oli Dagenais <olivida@microsoft.com>
2015-10-22 10:46:37 -0400

    Add ctor overload to NotImplementedException
    This is to encourage adding a workitem to track the missing
    functionality.

--- src/main/java/com/microsoft/alm/helpers/NotImplementedException.java
+++ src/main/java/com/microsoft/alm/helpers/NotImplementedException.java
@@ -5,7 +5,20 @@ package com.microsoft.alm.helpers;
 
 public class NotImplementedException extends RuntimeException
 {
+    private final int workItemNumber;
+
     public NotImplementedException()
     {
+        this(-1);
+    }
+
+    public NotImplementedException(final int workItemNumber)
+    {
+        this.workItemNumber = workItemNumber;
+    }
+
+    public int getWorkItemNumber()
+    {
+        return workItemNumber;
     }
 }
