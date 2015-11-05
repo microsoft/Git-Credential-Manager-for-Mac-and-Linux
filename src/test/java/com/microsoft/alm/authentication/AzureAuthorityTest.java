microsoft/Git-Credential-Manager-for-Mac-and-Linux

commit 8f9e3aba21d5f146398dbe5282ca1d9f3833e4c8

Refactor: flip testing of equality with Guid.Empty

src/main/java/com/microsoft/alm/authentication/AzureAuthority.java
@@ -197,7 +197,7 @@ class AzureAuthority implements IAzureAuthority
             qs.put(OAuthParameter.LOGIN_HINT, userId.getId());
         }
 
-        if (correlationId != null && !correlationId.equals(Guid.Empty))
+        if (correlationId != null && !Guid.Empty.equals(correlationId))
         {
             qs.put(OAuthParameter.CORRELATION_ID, correlationId.toString());
         }
@@ -264,7 +264,7 @@ class AzureAuthority implements IAzureAuthority
         qs.put(OAuthParameter.GRANT_TYPE, OAuthParameter.AUTHORIZATION_CODE);
         qs.put(OAuthParameter.CODE, authorizationCode);
         qs.put(OAuthParameter.REDIRECT_URI, redirectUri.toString());
-        if (correlationId != null && !correlationId.equals(Guid.Empty))
+        if (correlationId != null && Guid.Empty.equals(correlationId))
         {
             qs.put(OAuthParameter.CORRELATION_ID, correlationId.toString());
             qs.put(OAuthParameter.REQUEST_CORRELATION_ID_IN_RESPONSE, "true");

src/main/java/com/microsoft/alm/authentication/BaseVsoAuthentication.java
@@ -321,7 +321,7 @@ public abstract class BaseVsoAuthentication extends BaseAuthentication
         if (detectAuthority(targetUri, tenantId))
         {
             // empty Guid is MSA, anything else is AAD
-            if (tenantId.get().equals(Guid.Empty))
+            if (Guid.Empty.equals(tenantId.get()))
             {
                 Trace.writeLine("   MSA authority detected");
                 authentication.set(new VsoMsaAuthentication(scope, personalAccessTokenStore, adaRefreshTokenStore));

commit ae20f4056ec4856019b264433be403ec6d1a2789

TDD: fix defect introduced in 8f9e3aba

src/main/java/com/microsoft/alm/authentication/AzureAuthority.java
@@ -275,7 +275,7 @@ class AzureAuthority implements IAzureAuthority
         qs.put(OAuthParameter.GRANT_TYPE, OAuthParameter.AUTHORIZATION_CODE);
         qs.put(OAuthParameter.CODE, authorizationCode);
         qs.put(OAuthParameter.REDIRECT_URI, redirectUri.toString());
-        if (correlationId != null && Guid.Empty.equals(correlationId))
+        if (correlationId != null && !Guid.Empty.equals(correlationId))
         {
             qs.put(OAuthParameter.CORRELATION_ID, correlationId.toString());
             qs.put(OAuthParameter.REQUEST_CORRELATION_ID_IN_RESPONSE, "true");

src/test/java/com/microsoft/alm/authentication/AzureAuthorityTest.java
@@ -97,4 +97,34 @@ public class AzureAuthorityTest
             "&code=" + authorizationCode +
             "&redirect_uri=https%3A%2F%2Fexample.com", actual.getContent());
     }
+
+    @Test
+    public void createTokenRequest_withCorrelationId() throws Exception
+    {
+        final String resource = "a8860e8f-ca7d-4efe-b80d-4affab13d4ba";
+        final String clientId = "f7e11bcd-b50b-4869-ad88-8bdd6cbc8473";
+        final UUID correlationId = UUID.fromString("519a4fa6-c18f-4230-8290-6c57407656c9");
+        // authorization codes can be pretty long
+        final String authorizationCode =
+            "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" +
+                "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" +
+                "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" +
+                "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" +
+                "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" +
+                "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" +
+                "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" +
+                "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+        final URI redirectUri = URI.create("https://example.com");
+
+        final StringContent actual = AzureAuthority.createTokenRequest(resource, clientId, authorizationCode, redirectUri, correlationId);
+
+        Assert.assertEquals(
+            "resource=a8860e8f-ca7d-4efe-b80d-4affab13d4ba" +
+                "&client_id=f7e11bcd-b50b-4869-ad88-8bdd6cbc8473" +
+                "&grant_type=authorization_code" +
+                "&code=" + authorizationCode +
+                "&redirect_uri=https%3A%2F%2Fexample.com" +
+                "&client-request-id=519a4fa6-c18f-4230-8290-6c57407656c9" +
+                "&return-client-request-id=true", actual.getContent());
+    }
 }