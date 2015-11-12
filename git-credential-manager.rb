a369b26003628baf6c1d6265cae7b2551d3197d6
leantk <leantk@microsoft.com>
2015-11-12 08:59:36 -0500

    Changing script to use URL from POM

--- git-credential-manager.rb
+++ git-credential-manager.rb
@@ -1,6 +1,6 @@
 class GitCredentialManager < Formula
   desc "Stores credentials for Git on Visual Studio Online (VSO)"
-  homepage "http://java.visualstudio.com/Docs/tools/intro"
+  homepage "${project.url}"
   url "https://github.com/Microsoft/Git-Credential-Manager-for-Mac-and-Linux/releases/download/git-credential-manager-${version}/git-credential-manager-${version}.jar"
   sha256 "TBD"
 
