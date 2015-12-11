f2b12e55fa44575f4f35a79693450fd1fef5f5a7
Oli Dagenais <olivida@microsoft.com>
2015-12-11 11:38:16 -0500

    Move credentialFromXml() to Credential.fromXml

--- src/main/java/com/microsoft/alm/authentication/Credential.java
+++ src/main/java/com/microsoft/alm/authentication/Credential.java
@@ -5,6 +5,9 @@ package com.microsoft.alm.authentication;
 
 import com.microsoft.alm.helpers.ObjectExtensions;
 import com.microsoft.alm.helpers.StringHelper;
+import com.microsoft.alm.helpers.XmlHelper;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import javax.xml.bind.DatatypeConverter;
 import java.nio.charset.Charset;
@@ -48,6 +51,32 @@ public final class Credential extends Secret
      * Unique identifier of the user.
      */
     public final String Username;
+
+    public static Credential fromXml(final Node credentialNode)
+    {
+        Credential value;
+        String password = null;
+        String username = null;
+
+        final NodeList propertyNodes = credentialNode.getChildNodes();
+        for (int v = 0; v < propertyNodes.getLength(); v++)
+        {
+            final Node propertyNode = propertyNodes.item(v);
+            if (propertyNode.getNodeType() != Node.ELEMENT_NODE) continue;
+
+            final String propertyName = propertyNode.getNodeName();
+            if ("Password".equals(propertyName))
+            {
+                password = XmlHelper.getText(propertyNode);
+            }
+            else if ("Username".equals(propertyName))
+            {
+                username = XmlHelper.getText(propertyNode);
+            }
+        }
+        value = new Credential(username, password);
+        return value;
+    }
 
     /**
      * Compares an object to this {@link Credential} for equality.
