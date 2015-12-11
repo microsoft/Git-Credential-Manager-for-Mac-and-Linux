f30d787f0f4337cad23eb5cf1e3be79d5804d0b4
Oli Dagenais <olivida@microsoft.com>
2015-12-11 11:30:27 -0500

    Add XmlHelper class skeleton

---
+++ src/main/java/com/microsoft/alm/helpers/XmlHelper.java
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root.
+
+package com.microsoft.alm.helpers;
+
+public class XmlHelper
+{
+}
