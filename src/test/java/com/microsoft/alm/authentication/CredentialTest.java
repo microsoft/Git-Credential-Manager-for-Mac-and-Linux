5837d85c5c6cacf7c984d8181b44553c5faae737
Oli Dagenais <olivida@microsoft.com>
2015-12-11 13:44:30 -0500

    Add skeleton CredentialTest

---
+++ src/test/java/com/microsoft/alm/authentication/CredentialTest.java
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root.
+
+package com.microsoft.alm.authentication;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CredentialTest
+{
+
+}
