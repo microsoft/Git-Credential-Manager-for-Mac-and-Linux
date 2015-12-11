ddfc3fc2d71f00924a0b6c69ae9339b5ba5901bb
Oli Dagenais <olivida@microsoft.com>
2015-12-11 12:32:50 -0500

    Add StringHelper.assertLinesEqual()

--- src/test/java/com/microsoft/alm/helpers/StringHelperTest.java
+++ src/test/java/com/microsoft/alm/helpers/StringHelperTest.java
@@ -6,6 +6,10 @@ package com.microsoft.alm.helpers;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+
 public class StringHelperTest
 {
     @Test public void endsWithIgnoreCase_positive()
@@ -155,5 +159,31 @@ public class StringHelperTest
         Assert.assertEquals(" trimEnd", StringHelper.trimEnd(" trimEnd "));
         Assert.assertEquals("", StringHelper.trimEnd(""));
         Assert.assertEquals("", StringHelper.trimEnd(" "));
+    }
+
+    public static void assertLinesEqual(final String expected, final String actual) throws IOException
+    {
+        final StringReader expectedSr = new StringReader(expected);
+        final BufferedReader expectedBr = new BufferedReader(expectedSr);
+        final StringReader actualSr = new StringReader(actual);
+        final BufferedReader actualBr = new BufferedReader(actualSr);
+
+        String expectedLine;
+        String actualLine;
+        while ((expectedLine = expectedBr.readLine()) != null)
+        {
+            if ((actualLine = actualBr.readLine()) != null)
+            {
+                Assert.assertEquals(expectedLine, actualLine);
+            }
+            else
+            {
+                Assert.fail("'expected' contained more lines than 'actual'.");
+            }
+        }
+        if ((actualLine = actualBr.readLine()) != null)
+        {
+            Assert.fail("'actual' contained more lines than 'expected'.");
+        }
     }
 }
