cedb82b4f74995e00345c5961aaef75c811bc20a
Oli Dagenais <olivida@microsoft.com>
2016-01-19 08:51:35 -0500

    Add createServiceName() method and calls

--- src/main/java/com/microsoft/alm/authentication/KeychainSecurityCliStore.java
+++ src/main/java/com/microsoft/alm/authentication/KeychainSecurityCliStore.java
@@ -7,34 +7,54 @@ import com.microsoft.alm.helpers.NotImplementedException;
 
 public class KeychainSecurityCliStore implements ISecureStore
 {
+    static final String PREFIX = "gcm4ml:";
+
+    /**
+     * Adds a prefix to the target name to avoid a collision
+     * with the built-in git-credential-osxkeychain.
+     * This is because the built-in helper will not validate the credentials first,
+     * leading to a poor user experience if the token is no longer valid.
+     *
+     * @param targetName the string provided to {@see ISecureStore} methods
+     * @return a string suitable for use as the "service name"
+     */
+    static String createServiceName(final String targetName)
+    {
+        return PREFIX + targetName;
+    }
 
     @Override
     public void delete(final String targetName)
     {
+        final String serviceName = createServiceName(targetName);
         throw new NotImplementedException(421274);
     }
 
     @Override
     public Credential readCredentials(final String targetName)
     {
+        final String serviceName = createServiceName(targetName);
         throw new NotImplementedException(421274);
     }
 
     @Override
     public Token readToken(final String targetName)
     {
+        final String serviceName = createServiceName(targetName);
         throw new NotImplementedException(421274);
     }
 
     @Override
     public void writeCredential(final String targetName, final Credential credentials)
     {
+        final String serviceName = createServiceName(targetName);
         throw new NotImplementedException(421274);
     }
 
     @Override
     public void writeToken(final String targetName, final Token token)
     {
+        final String serviceName = createServiceName(targetName);
         throw new NotImplementedException(421274);
     }
 }
