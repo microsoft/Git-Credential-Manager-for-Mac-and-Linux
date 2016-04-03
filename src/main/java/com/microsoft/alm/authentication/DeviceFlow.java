9212b5f7aa5bebe2052f38d33c05c45bb2274329
Oli Dagenais <olivida@microsoft.com>
2016-04-03 14:54:25 -0400

    Initial design of the Device Flow implementation
    Based on draft 01 of "OAuth 2.0 Device Flow" at
    https://tools.ietf.org/html/draft-ietf-oauth-device-flow-01

---
+++ src/main/java/com/microsoft/alm/authentication/DeviceFlow.java
@@ -0,0 +1,52 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root.
+
+package com.microsoft.alm.authentication;
+
+import com.microsoft.alm.oauth2.useragent.AuthorizationException;
+
+import java.net.URI;
+
+/**
+ * An object that implements part of the OAuth 2.0 Device Flow.
+ *
+ * @see <a href="https://tools.ietf.org/html/draft-ietf-oauth-device-flow-01">
+ *     OAuth 2.0 Device Flow IETF Draft 01
+ *     </a>
+ */
+public interface DeviceFlow
+{
+    /**
+     * The client initiates the flow by requesting a set of verification
+     * codes from the authorization server's device endpoint.
+     *
+     * @param deviceEndpoint the authorization server's endpoint capable of issuing
+     *                       verification codes, user codes, and verification URLs.
+     * @param clientId       the client identifier as described in Section 2.2 of RFC6749.
+     * @param scope          the scope of the access request as described by
+     *                       Section 3.3 of RFC6749. (optional)
+     * @return               a {@link DeviceFlowResponse} representing how the 2nd
+     *                       phase of the protocol should proceed.
+     *
+     * @see                  "steps (A) and (B) of the Device Flow"
+     */
+    DeviceFlowResponse requestAuthorization(final URI deviceEndpoint, final String clientId, final String scope);
+
+    /**
+     * The client polls the authorization server's token endpoint repeatedly
+     * until the end-user grants or denies the request, or the verification
+     * code expires.
+     *
+     * @param tokenEndpoint           the authorization server's
+     *                                token endpoint as described in Section 4.1.1 of RFC6749.
+     * @param clientId                the client identifier as described in Section 2.2 of RFC6749.
+     * @param deviceFlowResponse      the response obtained from
+     *                                {@link #requestAuthorization(URI, String, String)}.
+     * @return                        a pair of tokens.
+     * @throws AuthorizationException the end-user denied the request,
+     *                                or the verification code expired.
+     *
+     * @see                           "steps (E) and (F) of the Device Flow"
+     */
+    TokenPair requestToken(final URI tokenEndpoint, String clientId, final DeviceFlowResponse deviceFlowResponse) throws AuthorizationException;
+}
---
+++ src/main/java/com/microsoft/alm/authentication/DeviceFlowResponse.java
@@ -0,0 +1,115 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root.
+
+package com.microsoft.alm.authentication;
+
+import java.net.URI;
+import java.util.Calendar;
+
+/**
+ * Represents the result of requesting device flow authorization from a Device Endpoint.
+ *
+ * @see "Section 3.1 Client Requests Authorization"
+ */
+public class DeviceFlowResponse
+{
+    private final String deviceCode;
+    private final String userCode;
+    private final URI verificationUri;
+    private final int expiresIn;
+    private final Calendar expiresAt;
+    private final int interval;
+
+    public DeviceFlowResponse(final String deviceCode, final String userCode, final URI verificationUri, final int expiresIn, final int interval)
+    {
+        this.deviceCode = deviceCode;
+        this.userCode = userCode;
+        this.verificationUri = verificationUri;
+        this.expiresIn = expiresIn;
+        this.expiresAt = Calendar.getInstance();
+        expiresAt.add(Calendar.SECOND, expiresIn);
+        this.interval = interval;
+    }
+
+    /**
+     * The "Device Verification Code" is defined as
+     * "A short-lived token representing an authorization session."
+     * in section 2.
+     *
+     * @return a string that is to be supplied to the token endpoint.
+     */
+    public String getDeviceCode()
+    {
+        return deviceCode;
+    }
+
+    /**
+     * The "End-User Verification Code" is defined as
+     * "A short-lived token which the device displays to the end user, is
+     * entered by the end-user on the authorization sever, and is thus
+     * used to bind the device to the end-user."
+     * in section 2.
+     *
+     * @return a string that the resource owner (end-user)
+     *         will type into the user-agent (web browser)
+     *         to link their authentication and authorization
+     *         with the device.
+     */
+    public String getUserCode()
+    {
+        return userCode;
+    }
+
+    /**
+     * The end-user verification URI on the authorization
+     * server.  The URI should be short and easy to remember as end-
+     * users will be asked to manually type it into their user-agent.
+     *
+     * @return the URI that the resource owner (end-user)
+     *         will visit with their user-agent (web browser)
+     *         to complete the device flow process.
+     */
+    public URI getVerificationUri()
+    {
+        return verificationUri;
+    }
+
+    /**
+     * The duration in seconds of the verification code
+     * lifetime.
+     *
+     * @return the number of seconds the resource owner (end-user)
+     *         has to complete the device flow process.
+     *
+     */
+    public int getExpiresIn()
+    {
+        return expiresIn;
+    }
+
+    /**
+     * The date and time when the verification code will
+     * no longer be valid.
+     *
+     * @return a Calendar representing the instant when the
+     *         resource owner (end-user) will no longer be
+     *         able to complete the device flow process.
+     *
+     */
+    public Calendar getExpiresAt()
+    {
+        return expiresAt;
+    }
+
+    /**
+     * The minimum amount of time in seconds that the client
+     * SHOULD wait between polling requests to the token endpoint.
+     *
+     * @return the minimum number of seconds the client (application)
+     *         waits before polling the token endpoint.
+     */
+    public int getInterval()
+    {
+        return interval;
+    }
+}
