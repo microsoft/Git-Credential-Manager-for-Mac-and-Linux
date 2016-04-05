267b7ddfbb588f1fb0a9c3fa4b80c7442463934e
Oli Dagenais <olivida@microsoft.com>
2016-04-05 14:33:59 -0400

    Add stub*() test helper methods

--- src/test/groovy/com/microsoft/alm/authentication/DeviceFlowImplTest.groovy
+++ src/test/groovy/com/microsoft/alm/authentication/DeviceFlowImplTest.groovy
@@ -24,6 +24,8 @@ public class DeviceFlowImplTest {
     private static final URI VERIFICATION_URI = new URI("http://verification.example.com");
     private static final int EXPIRY_SECONDS = 600;
     private static final int ATTEMPT_INTERVAL = 1;
+    private static final String DEVICE_ENDPOINT_PATH = "/device";
+    private static final String TOKEN_ENDPOINT_PATH = "/token";
     private static final DeviceFlowResponse DEFAULT_DEVICE_FLOW_RESPONSE = new DeviceFlowResponse(DEVICE_CODE, USER_CODE, VERIFICATION_URI, EXPIRY_SECONDS, ATTEMPT_INTERVAL);
 
     private final String host;
@@ -35,10 +37,113 @@ public class DeviceFlowImplTest {
         host = localHostAddress.hostName;
     }
 
+    private void stubDeviceEndpoint(final int interval = ATTEMPT_INTERVAL, final int expiresIn = -1, final String requestBodySuffix = "", String responseBodyPrefix = "") {
+        final def deviceRequestBody = "response_type=device_code&client_id=${CLIENT_ID}" + requestBodySuffix;
+        if (interval > 0) {
+            responseBodyPrefix += /"interval":${interval},
+/
+        }
+        if (expiresIn > 0) {
+            responseBodyPrefix += /"expires_in":${expiresIn},
+/
+        }
+        final def deviceResponseBody = """\
+{
+    ${responseBodyPrefix}
+    "device_code":"${DEVICE_CODE}",
+    "user_code":"${USER_CODE}",
+    "verification_uri":"${VERIFICATION_URI}"
+}
+""";
+
+        stubFor(
+            post(
+                urlEqualTo(DEVICE_ENDPOINT_PATH)
+            )
+            .withRequestBody(
+                equalTo(deviceRequestBody)
+            )
+            .willReturn(
+                aResponse()
+                .withStatus(200)
+                .withHeader("Content-Type", "application/json")
+                .withHeader("Cache-Control", "no-store")
+                .withBody(deviceResponseBody)
+            )
+        );
+    }
+
+    private void stubTokenEndpointSuccess(final String requestBodySuffix = "", String responseBodyPrefix = "") {
+        final def tokenRequestBody = "grant_type=device_code&code=${DEVICE_CODE}&client_id=${CLIENT_ID}" + requestBodySuffix;
+        final def tokenResponseBody = """\
+{
+    ${responseBodyPrefix}
+    "access_token":"${ACCESS_TOKEN}",
+    "token_type":"bearer",
+    "expires_in":3600
+}
+""";
+        stubFor(
+            post(
+                urlEqualTo(TOKEN_ENDPOINT_PATH)
+            )
+            .withRequestBody(
+                equalTo(tokenRequestBody)
+            )
+            .willReturn(
+                aResponse()
+                .withStatus(200)
+                .withHeader("Content-Type", "application/json;charset=UTF-8")
+                .withHeader("Cache-Control", "no-store")
+                .withHeader("Pragma", "no-cache")
+                .withBody(tokenResponseBody)
+            )
+        );
+    }
+
+    private void stubTokenEndpointError(final String requestBody, final String errorCode, final String errorDescription = null, final URI errorUri = null) {
+
+        final def tokenRequestBody = requestBody;
+
+        def responseBodyPrefix = ""
+        if (errorDescription) {
+            responseBodyPrefix += /"error_description": "${errorDescription}",
+/
+        }
+        if (errorUri != null) {
+            responseBodyPrefix += /"error_uri": "${errorUri}",
+/
+        }
+
+        final def tokenResponseBody = """\
+{
+    ${responseBodyPrefix}
+    "error":"${errorCode}"
+}
+""";
+
+        stubFor(
+            post(
+                urlEqualTo(TOKEN_ENDPOINT_PATH)
+            )
+            .withRequestBody(
+                equalTo(tokenRequestBody)
+            )
+            .willReturn(
+                aResponse()
+                .withStatus(400)
+                .withHeader("Content-Type", "application/json;charset=UTF-8")
+                .withHeader("Cache-Control", "no-store")
+                .withHeader("Pragma", "no-cache")
+                .withBody(tokenResponseBody)
+            )
+        );
+    }
+
     @Test public void endToEnd_authorizedRightAway() {
         final def port = wireMockRule.port();
-        final def deviceEndpoint = new URI(PROTOCOL, null, host, port, "/device", null, null);
-        final def tokenEndpoint = new URI(PROTOCOL, null, host, port, "/token", null, null);
+        final def deviceEndpoint = new URI(PROTOCOL, null, host, port, DEVICE_ENDPOINT_PATH, null, null);
+        final def tokenEndpoint = new URI(PROTOCOL, null, host, port, TOKEN_ENDPOINT_PATH, null, null);
         final def cut = new DeviceFlowImpl();
 
         final def actualResponse = cut.requestAuthorization(deviceEndpoint, CLIENT_ID, null);
