a7672573cd99e3ef090a695949864f2a2de4ebe1
Oli Dagenais <olivida@microsoft.com>
2016-04-05 10:24:07 -0400

    Preliminary e2e test w/hardcoded implementation

--- src/main/java/com/microsoft/alm/authentication/DeviceFlowImpl.java
+++ src/main/java/com/microsoft/alm/authentication/DeviceFlowImpl.java
@@ -12,12 +12,12 @@ public class DeviceFlowImpl implements DeviceFlow
     @Override
     public DeviceFlowResponse requestAuthorization(final URI deviceEndpoint, final String clientId, final String scope)
     {
-        return null;
+        return new DeviceFlowResponse("9297fb18-46d0-4846-97ca-ab8dd3b55729", "A1B2B4C1C5D1D3E3E5", URI.create("http://verification.example.com"), 600, 1);
     }
 
     @Override
     public TokenPair requestToken(final URI tokenEndpoint, final String clientId, final DeviceFlowResponse deviceFlowResponse) throws AuthorizationException
     {
-        return null;
+        return new TokenPair("d15281b1-03f1-4581-90d3-4527d9cf4147", "fake-refresh-token");
     }
 }
--- src/test/groovy/com/microsoft/alm/authentication/DeviceFlowImplTest.groovy
+++ src/test/groovy/com/microsoft/alm/authentication/DeviceFlowImplTest.groovy
@@ -8,6 +8,8 @@ import groovy.transform.CompileStatic
 import org.junit.Rule
 import org.junit.Test
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
 /**
  * A class to test {@see DeviceFlowImpl}.
  */
@@ -15,6 +17,7 @@ import org.junit.Test
 public class DeviceFlowImplTest {
 
     private static final String PROTOCOL = "http";
+    private static final String CLIENT_ID = "contoso";
     private static final String DEVICE_CODE = "9297fb18-46d0-4846-97ca-ab8dd3b55729";
     private static final String USER_CODE = "A1B2B4C1C5D1D3E3E5";
     private static final String ACCESS_TOKEN = "d15281b1-03f1-4581-90d3-4527d9cf4147";
@@ -30,6 +33,27 @@ public class DeviceFlowImplTest {
     public DeviceFlowImplTest() {
         final def localHostAddress = InetAddress.localHost;
         host = localHostAddress.hostName;
+    }
+
+    @Test public void endToEnd_authorizedRightAway() {
+        final def port = wireMockRule.port();
+        final def deviceEndpoint = new URI(PROTOCOL, null, host, port, "/device", null, null);
+        final def tokenEndpoint = new URI(PROTOCOL, null, host, port, "/token", null, null);
+        final def cut = new DeviceFlowImpl();
+
+        final def actualResponse = cut.requestAuthorization(deviceEndpoint, CLIENT_ID, null);
+
+        assert DEVICE_CODE == actualResponse.deviceCode;
+        assert USER_CODE == actualResponse.userCode;
+        assert VERIFICATION_URI == actualResponse.verificationUri;
+        assert EXPIRY_SECONDS == actualResponse.expiresIn;
+        assert ATTEMPT_INTERVAL == actualResponse.interval;
+
+        final def actualTokenPair = cut.requestToken(tokenEndpoint, CLIENT_ID, actualResponse);
+
+        final def actualAccessToken = actualTokenPair.AccessToken;
+        assert TokenType.Access == actualAccessToken.Type;
+        assert ACCESS_TOKEN == actualAccessToken.Value;
     }
 
 }
