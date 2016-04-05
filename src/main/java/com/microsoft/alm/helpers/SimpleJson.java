da4c82b515814631331ff487e5ae08f728ca0087
Oli Dagenais <olivida@microsoft.com>
2016-04-05 14:41:55 -0400

    Add SimpleJson[Test] skeletons

---
+++ src/main/java/com/microsoft/alm/helpers/SimpleJson.java
@@ -0,0 +1,21 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root.
+
+package com.microsoft.alm.helpers;
+
+import java.util.Map;
+
+/**
+ * A very simple JSON [de-]serializer that only handles a dictionary of scalars.
+ */
+public class SimpleJson {
+
+    public static Map<String, Object> parse(final String input) {
+        return null;
+    }
+
+    public static String format(final Map<String, Object> input) {
+        return null;
+    }
+
+}
