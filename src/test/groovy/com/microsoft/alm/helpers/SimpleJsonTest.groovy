03832c645e854a341aa1b5180b1544a15c32513c
Oli Dagenais <olivida@microsoft.com>
2016-04-05 21:49:20 -0400

    Implement parse() with exhaustive tests

--- src/test/groovy/com/microsoft/alm/helpers/SimpleJsonTest.groovy
+++ src/test/groovy/com/microsoft/alm/helpers/SimpleJsonTest.groovy
@@ -4,6 +4,7 @@
 package com.microsoft.alm.helpers
 
 import groovy.transform.CompileStatic
+import org.junit.Assert
 import org.junit.Test
 
 /**
@@ -18,6 +19,18 @@ public class SimpleJsonTest {
         assert expected == actual
     }
 
+    private static void assertParseError(final String expectedMessage, final String input) {
+        try {
+            SimpleJson.parse(input)
+        }
+        catch (final IllegalArgumentException actual) {
+            def actualMessage = actual.message
+            assert expectedMessage == actualMessage
+            return;
+        }
+        Assert.fail("Expected IllegalArgumentException with message: " + expectedMessage)
+    }
+
     @Test public void parse_emptyString() {
         assertParse([:], "")
     }
@@ -26,4 +39,108 @@ public class SimpleJsonTest {
         assertParse([:], "{}")
     }
 
+    @Test public void parse_singleString() {
+        assertParse(["name":"value"], /{"name":"value"}/)
+        assertParse(["name":"value"], /{"name":"value",}/)
+    }
+
+    @Test public void parse_insignificantWhitespace() {
+        assertParse(["name":"value"], /{"name":"value" ,}/)
+        assertParse(["name":"value"], /{"name":"value", }/)
+        assertParse(["name":"value"], /{"name" :"value"}/)
+        assertParse(["name":"value"], '{\t"name"\r:\t"value"\n}')
+    }
+
+    @Test public void parse_escapedString() {
+        assertParse(["name":"/\b\f\"\n\r\t\u20AC\\"], '{"name":"\\/\\b\\f\\"\\n\\r\\t\\u20AC\\\\"}')
+    }
+
+    @Test public void parse_singleNumber() {
+        assertParse(["answer":42], /{"answer":42}/)
+        assertParse(["answer":42], /{"answer":42,}/)
+        assertParse(["answer":42], /{"answer":42 ,}/)
+    }
+
+    @Test public void parse_singleNegativeNumber() {
+        assertParse(["answer":-42], /{"answer":-42}/)
+    }
+
+    @Test public void parse_singleFractionalNumber() {
+        assertParse(["answer":4.2], /{"answer":4.2}/)
+    }
+
+    @Test public void parse_singleExponentialNumber() {
+        assertParse(["answer":4e1], /{"answer":4e1}/)
+        assertParse(["answer":4e1], /{"answer":4E1}/)
+        assertParse(["answer":4e1], /{"answer":4E+1}/)
+        assertParse(["answer":4e-1], /{"answer":4E-1}/)
+    }
+
+    @Test public void parse_singleLiteral() {
+        assertParse(["answer":true], /{"answer":true}/)
+        assertParse(["answer":true], /{"answer":true,}/)
+        assertParse(["answer":false], /{"answer":false}/)
+        assertParse(["answer":false], /{"answer":false,}/)
+        assertParse(["answer":null], /{"answer":null}/)
+        assertParse(["answer":null], /{"answer":null,}/)
+    }
+
+    @Test public void parse_deviceEndpointExampleResponse() {
+        final input = """
+  {
+    "device_code":"74tq5miHKB",
+    "user_code":"94248",
+    "verification_uri":"http://www.example.com/device",
+    "interval":5
+  }
+"""
+        final def expected = [
+            "device_code":"74tq5miHKB",
+            "user_code":"94248",
+            "verification_uri":"http://www.example.com/device",
+            "interval":5
+        ]
+
+        assertParse(expected, input)
+    }
+
+    @Test public void parse_error_START() {
+        assertParseError("Unexpected character '[' at state START.", /[/)
+    }
+
+    @Test public void parse_error_PRE_KEY() {
+        assertParseError("Unexpected character '[' at state PRE_KEY.", /{[/)
+    }
+
+    @Test public void parse_error_PRE_VALUE() {
+        assertParseError("Unexpected character '=' at state PRE_VALUE.", /{"key"=/)
+    }
+
+    @Test public void parse_error_VALUE() {
+        assertParseError("Unexpected character '=' at state VALUE.", /{"key":=/)
+    }
+
+    @Test public void parse_error_NUMBER_VALUE() {
+        assertParseError("Unexpected character 'a' at state NUMBER_VALUE.", /{"key":3a/)
+    }
+
+    @Test public void parse_error_STRING_VALUE_ESCAPE() {
+        assertParseError("Unexpected character '?' at state STRING_VALUE_ESCAPE.", /{"key":"\?/)
+    }
+
+    @Test public void parse_error_STRING_VALUE_UNICODE() {
+        assertParseError("Unexpected character 'x' at state STRING_VALUE_UNICODE.", '{"key":"\\ux')
+    }
+
+    @Test public void parse_error_LITERAL_VALUE() {
+        assertParseError("Unexpected character 't' at state LITERAL_VALUE.", /{"key":tt/)
+    }
+
+    @Test public void parse_error_POST_VALUE() {
+        assertParseError("Unexpected character ';' at state POST_VALUE.", /{"key":"value";/)
+    }
+
+    @Test public void parse_error_END() {
+        assertParseError("Unexpected character ';' at state END.", /{"key":"value"};/)
+    }
 }
