26d9a7e8f6ab5ea8fc989d89874dcb49cdbd2361
Oli Dagenais <olivida@microsoft.com>
2016-05-13 16:13:23 -0400

    Fix debug assertion in DeviceFlowImplTest
    When running with the "debug" system property set to "true",
    some tests would fail due to Token wanting a non-null value
    in its constructor.

--- src/test/groovy/com/microsoft/alm/authentication/DeviceFlowImplTest.groovy
+++ src/test/groovy/com/microsoft/alm/authentication/DeviceFlowImplTest.groovy
@@ -31,6 +31,7 @@ public class DeviceFlowImplTest {
     private static final String DEVICE_CODE = "9297fb18-46d0-4846-97ca-ab8dd3b55729";
     private static final String USER_CODE = "A1B2B4C1C5D1D3E3E5";
     private static final String ACCESS_TOKEN = "d15281b1-03f1-4581-90d3-4527d9cf4147";
+    private static final String REFRESH_TOKEN = "08897313-7270-46d3-ac48-5577fa70e79f";
     private static final URI VERIFICATION_URI = new URI("http://verification.example.com");
     private static final int EXPIRY_SECONDS = 600;
     private static final int ATTEMPT_INTERVAL = 1;
@@ -122,6 +123,7 @@ public class DeviceFlowImplTest {
 {
     ${responseBodyPrefix}
     "access_token":"${ACCESS_TOKEN}",
+    "refresh_token":"${REFRESH_TOKEN}",
     "token_type":"bearer",
     "expires_in":3600
 }
